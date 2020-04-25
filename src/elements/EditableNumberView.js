212062b2eb5dd8cf9d40d3839d3f4f374a462b92
김지섭 <0226daniel@gmail.com>
2020-04-26 04:22:20 +0900

    Split Editable Number and Text

---
+++ src/elements/EditableNumberView.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import NumberFormat from 'react-number-format'
+import './EditableNumberView.scss'
+
+export default function (props) {
+	let view
+
+	if (props.editMode) {
+		view = (
+			<NumberFormat
+				thousandSeparator={true}
+				placeholder={props.label}
+				defaultValue={props.value}
+				onValueChange={(values) => {
+					if (props.onValueChange) props.onValueChange(values)
+				}}
+			/>
+		)
+	} else {
+		view = <NumberFormat value={props.value} thousandSeparator={true} displayType={'text'} />
+	}
+
+	return (
+		<p className="EditableNumberView" style={props.style}>
+			{view}
+		</p>
+	)
+}
---
+++ src/elements/EditableTextView.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './EditableTextView.scss'
+
+export default function (props) {
+	let view
+
+	if (props.editMode) {
+		view = (
+			<input
+				placeholder={props.label}
+				defaultValue={props.text}
+				onChange={(e) => {
+					if (props.onChange) props.onChange(e)
+				}}
+			/>
+		)
+	} else {
+		view = props.text
+	}
+
+	return (
+		<p className="EditableTextView" style={props.style}>
+			{view}
+		</p>
+	)
+}
